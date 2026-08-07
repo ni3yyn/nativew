c1a584a2ad5671604f1360bccf6915a8b4762ad6
unknown <xtra.alem@gmail.com>
2026-08-07 14:38:25 +0100

    aab fxd shrcmpnnt clms bbl fix 3 yml file old

--- babel.config.js
+++ babel.config.js
@@ -8,5 +8,16 @@ module.exports = function (api) {
       ['@babel/plugin-transform-private-methods', { loose: true }],
       ['@babel/plugin-transform-private-property-in-object', { loose: true }]
     ],
+    // 🌟 THIS IS THE FIX: Force Babel to translate #private variables inside node_modules
+    overrides: [
+      {
+        test: /node_modules/,
+        plugins: [
+          ['@babel/plugin-transform-class-properties', { loose: true }],
+          ['@babel/plugin-transform-private-methods', { loose: true }],
+          ['@babel/plugin-transform-private-property-in-object', { loose: true }]
+        ]
+      }
+    ]
   };
 };
