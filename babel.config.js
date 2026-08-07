ba57216998d00d3aa48766454667a42e2851be21
unknown <xtra.alem@gmail.com>
2026-08-07 15:11:19 +0100

    aab bbl fix 9 smpl bbl

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,6 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'babel-plugin-transform-import-meta',
-      // 🌟 1. Strip TypeScript syntax FIRST (declare class, types, etc.)
-      ['@babel/plugin-transform-typescript', { isTSX: true, allExtensions: true, allowDeclareFields: true }],
-      // 🌟 2. Then convert class properties and private (#) fields for Hermes
-      ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }]
-    ]
+    ],
   };
 };
